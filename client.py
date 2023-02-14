77a0ba3b863597a3deaa13f0948cf3ea7363bfc1
embo <emma.hakonsen@gmail.com>
2023-02-14 22:39:35 +0100

    Rewrote client and server using new skeleton code provided. Implemented broadcast function to notify of new clients joining server, but need to adjust so that the client joining doesn't receive the message as well.

--- client.py
+++ client.py
@@ -1,12 +1,50 @@
-from socket import *        #Imports socket library
+#Client side connects to the server and sends a message to everyone
 
-msg = "Hello world!"
-client_sd = socket(AF_INET, SOCK_STREAM)    #See server.py
-server_ip = gethostbyname(gethostname())   #Gets IP of your computer
-port = 17979    #Port that is used on server
-client_sd.connect((server_ip, port))     #Connects socket to given IP and port
-client_sd.send(msg.encode())     #Send encoded message
-received_line = client_sd.recv(1024).decode()   #Read data from the socket
-print(received_line)    #Print message
-client_sd.close()   #Close socket client side
+import socket
+import select
+import sys
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# write server ip and port, and connect
+### write your code here ###
+serverName = socket.gethostbyname(socket.gethostname())
+serverPort = 17979
+client_socket.connect((serverName,serverPort))
+### your code ends here ###
+
+while True:
+    """ we are going to use a select-based approach here because it will help
+    us deal with two inputs (user's input (stdin) and server's messages from 
+    socket)
+    """
+    inputs = [socket.socket(), client_socket]
+    """ read the select documentations - You pass select three lists: the 
+    first contains all sockets that you might want to try reading; the 
+    second all the sockets you might want to try writing to, and the last 
+    (normally left empty) those that you want to check for errors. """
+    read_sockets, write_socket, error_socket = select.select(inputs,[],[])
+    # we check if the message is either coming from your terminal or
+    # from a server
+    for socks in read_sockets:
+        if socks == client_socket:
+            # receive message from client and display it on the server side
+            # also handle exceptions here if there is no message from the
+            # client, you should exit.
+            ### write your code here ###
+            data = client_socket.recv(1024)
+            if not data:
+                print("Disconnected from server")
+                break
+            else:
+                print("New message: " + str(data, 'UTF-8'))      #print data
+            ### your code ends here ###
+        else:
+            #takes inputs from the user
+            message = input("write message: ")
+            #send a message to the server
+            ### write your code here ###
+            client_socket.send(message.encode())
+            ### your code ends here ###
+client_socket.close()
 
--- server.py
+++ server.py
@@ -1,16 +1,75 @@
-from socket import * # Imports socket library
+"""
+Server side: it simultaneously handles multiple clients
+and broadcasts when a new client joins or a client
+sends a message.
+"""
 
-server_sd = socket(AF_INET, SOCK_STREAM) #Creates socket, AF_INET -> Address family (IPV4) - SOCK_STREAM -> TCP Socket
-server_ip = gethostbyname(gethostname())   #Gets IP of your computer
-print(server_ip)
-port = 17979    #Port can be whatever you want, find one that is not used by other services
+import socket
+import _thread as thread
+import time
+import sys
 
-server_sd.bind((server_ip,port))     #Assigns IP and port to the created socket
-server_sd.listen()     #Starts listening for connections
-conn_sd, addr = server_sd.accept() #Server waits for connections, accepts any incoming
-received_line = conn_sd.recv(1024).decode() #Receives and decodes message
-print(received_line)  #Prints received message
-conn_sd.send(received_line.encode())    #Send message back to client
-#Close sockets server side
-conn_sd.close()
-server_sd.close()
+#this is to keep all the newly joined connections!
+all_client_connections = []
+def now():
+    return time.ctime(time.time())      # returns the time of day
+def handleClient(connection, addr):     # a client handler function
+    #this is where we broadcast everyone that a new client has joined
+
+    ### Write your code here ###
+    # append this to the list for broadcast
+    # create a message to inform all other clients that a new client has just joined
+    all_client_connections.append(connection)
+    print(*all_client_connections)
+    join_msg = "New user " + str(addr) + " has just joined the server!"
+    print(join_msg)
+    broadcast(connection, join_msg)
+    ### Your code ends here ###
+
+    while True:
+        message = connection.recv(2048).decode()
+        print(now() + " " + str(addr) + "#  ", message)
+        if (message == "exit" or not message):
+            break
+        ### Write your code here ###
+        #broadcast this message to the others
+        broadcast(connection, message)
+        ### Your code ends here ###
+    connection.close()
+    all_client_connections.remove(connection)
+def broadcast(connection, message):
+    print ("Broadcasting")
+    ### Write your code here ###
+    for connection in all_client_connections:
+        connection.send(message.encode())
+    ### Your code ends here ###
+def main():
+    """
+    creates a server socket, listens for new connections,
+    and spawns a new thread whenever a new connection join
+    """
+    serverName = socket.gethostbyname(socket.gethostname())
+    serverPort = 17979
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        # Use the bind function wisely!
+        ### Write your code here ###
+        serverSocket.bind(('',serverPort))
+        ### Your code ends here ###
+    except:
+        print("Bind failed. Error : ")
+        sys.exit()
+    serverSocket.listen(10)
+    print('The server is ready to receive')
+    while True:
+        ### Write your code here ###
+        (connectionSocket, addr) = serverSocket.accept()  # accept a connection
+        ### You code ends here ###
+
+        print('Server connected by ', addr)
+        print('at ', now())
+        thread.start_new_thread(handleClient, (connectionSocket,addr))
+    serverSocket.close()
+
+if __name__ == '__main__':
+    main()
