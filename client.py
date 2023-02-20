cb2cd966ea9d03a03c23c94adec2d8734ce7fd36
emmah <emma.hakonsen@gmail.com>
2023-02-20 10:01:54 +0100

    Improved broadcast function so that it can be used for all types of message.

--- client.py
+++ client.py
@@ -8,33 +8,31 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #write server ip and port, and connect
 ###write your code here###
-serverName = socket.gethostbyname(socket.gethostname())
-serverPort = 17979
-client_socket.connect((serverName, serverPort))
+serverName = socket.gethostbyname(socket.gethostname()) #identify the IP address of the server
+serverPort = 17979                                      #determines which server port the client can connect to
+client_socket.connect((serverName, serverPort))         #establish connection to server
 ###your code ends here###
 
 while True:
     inputs = [sys.stdin, client_socket]
     read_sockets, write_socket, error_socket = select.select(inputs,[],[])
-    #we check if the message is either coming from your terminal or from as server
+    #we check if the message is either coming from your terminal or from the server
     for socks in read_sockets:
         if socks == client_socket:
             #receive message from client and display it on the server side
             #also handle exceptions here if there is no message from the client,
             #you should exit
             ###write your code here###
-            data = client_socket.recv(1024)
-            if not data:
+            data = client_socket.recv(1024)             #message received from client stored in variable
+            if not data:                                #exit if-statement if no message received from client
                 break
             else:
-                print("New message: " + str(data, 'UTF-8'))
+                print("New message : " + str(data, 'UTF-8'))    #converts message to string and prints to screen
             ###your code ends here###
         else:
-            #takes input from the user
-            message = sys.stdin.readline()
-            #send a message to the server
             ###write your code here###
-            client_socket.send(message.encode())
+            message = sys.stdin.readline()              #take input from the user
+            client_socket.send(message.encode())        #send a message to the server
             ###your code ends here###
 
 client_socket.close()
--- server.py
+++ server.py
@@ -18,41 +18,30 @@ def now():
 def handleClient(connection, addr):     #a client handler function
     #this is where we broadcast everyone that a new client has joined
     ###write your code here###
-    #append this to the list for broadcast
-    #create a message to inform all other clients that a new client has just jo
-    all_client_connections.append(connection)
-    print(*all_client_connections, sep="\n")
-    join_msg = "New user " + str(addr) + " has just joined the server!"
-    print(join_msg)
-    broadcast2(connection, join_msg)
+    all_client_connections.append(connection)   #append new connection to the list for broadcast
+    print(*all_client_connections, sep="\n")    #prints updated list to screen
+    join_msg = "New user " + str(addr) + " has just joined the server!"     #message to inform all other clients that a new client has just joined
+    broadcast(all_client_connections[:-1], join_msg)    #broadcast new client message to all clients except the one who has just joined
     ###your code ends here###
 
     while True:
         message = connection.recv(2048).decode()
         print(now() + " " + str(addr) + "# ",message)
         if ("exit" in message or not message):
-            broadcast(connection, f'{addr} left the server')
+            broadcast(all_client_connections, f'{addr} left the server')
             connection.close()
             all_client_connections.remove(connection)
-            print(*all_client_connections, sep="\n")
+            print(*all_client_connections, sep="\n")    #prints updated list to screen
             break
         ###write your code here###
-        #broadcast this message to the others
-        broadcast(connection, message)
+        broadcast(all_client_connections, message)       #broadcast new message to all clients in the list
         ###your code ends here###
 
-def broadcast(connection, message):
+def broadcast(all_client_connections, message):
     print("Broadcasting")
     ###write your code here###
-    for connection in all_client_connections:
-        connection.send(message.encode())
-    ###your code ends here###
-
-def broadcast2(connection, message):
-    print("Broadcasting")
-    ###write your code here###
-    for connection in all_client_connections[:-1]:
-        connection.send(message.encode())
+    for connection in all_client_connections:       #iterates over all clients in list
+        connection.send(message.encode())           #sends encoded message to each client
     ###your code ends here###
 
 def main():
@@ -66,16 +55,16 @@ def main():
     try:
         #use the bind function wisely!
         ###write your code here###
-        serverSocket.bind(('',serverPort))
+        serverSocket.bind((serverName,serverPort))      #try to bind server socket to specific IP address/port
         #your code ends here
     except:
-        print("Bind failed. Error : ")
+        print("Bind failed. Error : ")                  
         sys.exit()
     serverSocket.listen(10)
     print("The server is ready to receive")
     while True:
         ###write your code here###
-        (connectionSocket, addr) = serverSocket.accept()
+        (connectionSocket, addr) = serverSocket.accept()    #accepts connect request from client socket
         ###your code ends here###
         print("Server connected by ", addr)
         print("at ", now())
